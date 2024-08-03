7c7fd183f988aa490f9db27c470fcc0a25c378f3
saura <sonisaurabh4@gmail.com>
2024-08-03 08:45:02 +0530

    Removed warnings.

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 # In-memory storage
 invoices = []
 
+
 @app.route('/invoices', methods=['POST'])
 def create_invoice():
     data = request.get_json()
@@ -17,9 +18,11 @@ def create_invoice():
     invoices.append(new_invoice)
     return jsonify({'message': 'Invoice created!'}), 201
 
+
 @app.route('/invoices', methods=['GET'])
 def get_invoices():
     return jsonify(invoices)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
